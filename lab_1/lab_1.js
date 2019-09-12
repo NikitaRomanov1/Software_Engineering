2eec743f16fdd1e13810e99896eba8cc3f8d7733
Nikita <nikita038rus@gmail.com>
2019-09-12 21:45:56 +0800

    exercise_1_3 full

--- lab_1/lab_1.js
+++ lab_1/lab_1.js
@@ -1,14 +1,20 @@
-
-const readline = require('readline');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+const readline = require('readline'); //подключение модуля node.js -readline
+const rl = readline.createInterface({ //установка интерфейса
+    input: process.stdin, //ввод данных
+    output: process.stdout //вывод данных
 });
-
+let exercise1_3 = {  //создание объекта
+    password : "qwerty",
+}
+exercise1_3.checking = function() {  //объявление метода
 rl.question('Enter the password: ', (answer) => {
-  if (answer != "qwerty") {
-  console.log(`Wrong password: ${answer}`);
-  rl.close(); } else {console.log(`Correct!`)
-  rl.close();}
-});
+    if (answer != this.password) { //проверка ввода пароля
+        console.log(`Wrong password: ${answer}`);
+        rl.close();
+    } else {
+        console.log(`Correct!`)
+        rl.close();
+    }
+}); 
+}
+exercise1_3.checking(); //вызов метода
