29cdfc2a5bde958fe0b0b0f6929a1d62a85c66e9
Nikita <nikita038rus@gmail.com>
2019-10-11 18:44:01 +0800

    lab_5 update

--- lab_5/lab_5.js
+++ lab_5/lab_5.js
@@ -1,13 +1,28 @@
-var fs = require('fs');
-var DOMParser = require('xmldom').DOMParser;
+const fs = require('fs');
+const DOMParser = require('xmldom').DOMParser;
 
 
 let text = fs.readFileSync("lab_5.xml", "utf8");
 
-var parser = new DOMParser();
-var xmlDom = parser.parseFromString(text,"text/xml");
+let parser = new DOMParser();
+let xmlDom = parser.parseFromString(text,"text/xml");
 
-for (let index = 0; index < xmlDom.getElementsByTagName('name').length; index++) {
-    var name = xmlDom.getElementsByTagName('name')[index].childNodes[0].nodeValue;
-    console.log(name);
-};
+const list = xmlDom.getElementsByTagName('tariff');
+let result = [];
+
+for (let index = 0; index < list.length; index++) {
+    let item = list.item(index);
+    let tariff = {};
+    tariff.name = item.getElementsByTagName('name').item(0).textContent;
+    tariff.operator_name = item.getElementsByTagName('operator_name').item(0).textContent;
+    tariff.payroll = item.getElementsByTagName('payroll').item(0).textContent;
+    tariff.call_prices_inside = item.getElementsByTagName('inside').item(0).textContent;
+    tariff.call_prices_outside = item.getElementsByTagName('outside').item(0).textContent;
+    tariff.call_prices_landline = item.getElementsByTagName('landline').item(0).textContent;
+    tariff.sms_price = item.getElementsByTagName('sms_price').item(0).textContent;
+    tariff.parameters_favourite_number = item.getElementsByTagName('favourite_number').item(0).textContent;
+    tariff.parameters_rate = item.getElementsByTagName('rate').item(0).textContent; 
+    tariff.parameters_connectionPayment = item.getElementsByTagName('connectionPayment').item(0).textContent;  
+    result.push(tariff);
+}
+console.log(result);
